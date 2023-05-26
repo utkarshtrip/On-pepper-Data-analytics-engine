3a8b3382c713d6d8b197a004b65511b5ba7f28bc
utkarshtrip <utkarsh.tripathi@flairminds.com>
2023-05-26 19:12:00 +0530

    url_updates

--- cal/urls.py
+++ cal/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('xbrl_table/', views.xbrl_table, name='xbrl_table'),
+    path('', views.xbrl_table, name='xbrl_table'),
     path('submit-expression', views.submit_expression, name="submit_expression"),
 ]
