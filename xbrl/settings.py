e6bb33b068a8747ca1150f5b8bc00d78a085db96
utkarshtrip <124048960+utkarshtrip@users.noreply.github.com>
2023-05-30 17:55:05 +0530

    Update settings.py

--- xbrl/settings.py
+++ xbrl/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    '/var/www/static/'
+#     '/var/www/static/'
 ]
 
 STATIC_ROOT = BASE_DIR / 'static_files'
