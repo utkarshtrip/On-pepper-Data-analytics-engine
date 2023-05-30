43d9762da20148793717d37d39929b4aca06b76b
utkarshtrip <utkarsh.tripathi@flairminds.com>
2023-05-30 11:37:27 +0530

    static root updated

--- cal/urls.py
+++ cal/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.xbrl_table, name='xbrl_table'),
     path('submit-expression', views.submit_expression, name="submit_expression"),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
--- xbrl/settings.py
+++ xbrl/settings.py
@@ -119,12 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = '/var/www/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
